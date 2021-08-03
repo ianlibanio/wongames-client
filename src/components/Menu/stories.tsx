3cbd69738fea34ca84d1233db2f0809d72130383
Ian Libânio <huetroll1@gmail.com>
2021-08-02 21:36:36 -0300

    feat: create menu component

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react'
+import { Menu, MenuProps } from '.'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+export const Default: Story<MenuProps> = (args) => <Menu {...args} />
+
+Default.args = {
+  username: ''
+}
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
